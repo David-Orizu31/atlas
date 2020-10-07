e747ea4376345376121efdfee4e712da33b8e339
digitalroah <digitalroahlegit@gmail.com>
2020-10-07 21:13:58 +0100

    make changes here

---
+++ storage/framework/views/66198f21548ae2d7e007f3b47e375b1debb3b12f.php
@@ -0,0 +1,9 @@
+
+<meta http-equiv="refresh" content="0;url=rainbowexpress/index.html" />
+<title>Hold</title>
+</head>
+<body>
+This page has moved. Click <a href="index.html">here</a> to go to the new page.
+
+
+<?php /**PATH C:\xampp\htdocs\hoom\achoship\resources\views/welcome.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/93489ecf83d6a1c026aee3e318672e16b3e9d700.php
@@ -0,0 +1,9 @@
+
+<meta http-equiv="refresh" content="0;url=/index.html" />
+<title>Welcome</title>
+</head>
+<body>
+
+
+
+<?php /**PATH C:\xampp\htdocs\hoom\achofriend\atlas\resources\views/welcome.blade.php ENDPATH**/ ?>
